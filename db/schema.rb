9972b5f70395c4242fed97479328371416db6b64
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-20 10:44:40 +0900

    テーブル作成（telephonesテーブル）

---
+++ db/schema.rb
@@ -0,0 +1,25 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171220013247) do
+
+  create_table "telephones", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "num"
+    t.bigint "telephone_type_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["telephone_type_id"], name: "index_telephones_on_telephone_type_id"
+    t.index ["user_id"], name: "index_telephones_on_user_id"
+  end
+
+end

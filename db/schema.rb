6ede6fedf00c6581c04710e13782870cf30a3346
R-Msb <ryu@MBP.local>
2017-12-21 13:08:21 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -71,5 +71,30 @@ ActiveRecord::Schema.define(version: 20171221005617) do
     t.datetime "updated_at", null: false
     t.index ["telephone_type_id"], name: "index_telephones_on_telephone_type_id"
     t.index ["user_id"], name: "index_telephones_on_user_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.date "birthday", null: false
+    t.string "address"
+    t.string "avatar"
+    t.string "facebook_url"
+    t.text "summary"
+    t.string "gender"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end

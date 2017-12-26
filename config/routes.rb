94f36327c00827d0582cb2199d91c6bb7d2f0bde
R-Msb <ryu@MBP.local>
2017-12-26 10:46:42 +0900

    add landings / update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
+  resources :landings, only: :index
+  root "landings#index"
 end

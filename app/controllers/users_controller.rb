83f9505f2b104717413ad9b847e230371f98f7fc
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2018-01-05 19:32:47 +0900

    users_controllerの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    @edit = User.new
+    @edit.save
   end
 
   def update

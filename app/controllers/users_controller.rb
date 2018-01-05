16ec4d57d20cc07f639f839d37f393c19a4969ff
コミットしました <yumayo14@gmail.com>
2018-01-06 08:47:23 +0900

    基本情報を登録するための設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,39 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = current_user
+    @company = Company.new
+  end
+
+  def create
+    @user = current_user
+    @company = Company.new(company_params)
+    @user.save
+    @company.save
+    redirect_to user_path
+  end
+
   def edit
-    @user = User.find(params[:id])
-    @edit = User.new
-    @edit.save
   end
 
   def update
   end
 
-  def index
+  private
+
+  def user_params
+    params.require(:user).permit(:birthday, :address, :facebook_url)
   end
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :companies
   resources :landings, only: :index
   root "landings#index"
 end

115da6d697a1a36987d4d4c0cf5068df56b3098a
コミットしました <yumayo14@gmail.com>
2018-01-12 12:43:06 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-  resources :companies
+  resources :users do
+    resources :academies
+    resources :careers
+    resources :companies
+  end
   resources :landings, only: :index
   root "landings#index"
 end

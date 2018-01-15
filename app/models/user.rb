ee549c2526723b8b796236814071c67909d8e452
R-Msb <ryu@MBP.local>
2018-01-15 12:09:14 +0900

    add association, def function

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ApplicationRecord
   has_many :companies, through: :careers
 
   has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :following_relationships, source: :followed
+
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end

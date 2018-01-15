ac9edaf575dc6c12d59aa219c4fca1ccb2462e98
花田暁 <hana9528van@gmail.com>
2018-01-15 17:29:10 +0900

    Create message model and messagess table

---
+++ db/migrate/20180115034234_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[5.1]
+  def change
+    create_table :messages do |t|
+      t.text    :message
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :send_user_id, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end

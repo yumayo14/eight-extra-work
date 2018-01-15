9fb1a2a189e8a1b7e747df673a394a8a824fe5ff
R-Msb <ryu@MBP.local>
2018-01-15 12:17:02 +0900

    add followed_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,18 @@ class User < ApplicationRecord
   has_many :careers
   has_many :companies, through: :careers
 
+  # one user has many follower_ids
+  # userAは複数のフォロー関係を持つ
   has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # one user has many followed_ids
+  # userAは複数の被フォロー関係を持つ
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # one user has many followed_ids through following_relationships
+  # userAは複数のfollowed_id(=userAがフォローしているuserid)を持つ
   has_many :following, through: :following_relationships, source: :followed
+  # one user has many follower_ids through followed_relationships
+  # userAは複数のfollower_id(=userAをフォローしているuserid)を持つ
+  has_many :followers, through: :followed_relationships, source: :follower
 
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)

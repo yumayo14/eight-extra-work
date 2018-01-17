216a7f9ed10d48f117fff5eaae6963387213c4d2
R-Msb <ryu@MBP.local>
2018-01-17 12:51:33 +0900

    update follow-action with ajax
    フォロー、アンフォローをajaxで実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,20 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(params[:followed_id])
+    @user = User.find(params[:followed_id])
     # userモデルのfollowメソッドを呼び出す
-    current_user.follow(current_user,user)
-    redirect_to user
+    current_user.follow(current_user,@user)
+    respond_to do |format|
+      format.html { redirect_to controller: :users, action: :profile, id: params[:followed_id] }
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(current_user,user)
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(current_user,@user)
+    respond_to do |format|
+      format.html { redirect_to controller: :users, action: :profile, id: params[:followed_id] }
+      format.js
+    end
   end
 end

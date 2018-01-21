08794866e8af84c68e84f600a76d034c41b4ecad
コミットしました <yumayo14@gmail.com>
2018-01-21 16:25:16 +0900

    Capistranoの設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-set :application, "eight_team-c"
-set :repo_url, "git@github.com:mashvalue1/eight_team-c.git"
+set :application, "eight-extra-work"
+set :repo_url, "git@github.com:yumayo14/eight-extra-work.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/ryu/.ssh/tech_key.pem']
+                  keys: ['/Users/yoshio/.ssh/yumayo14.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@
 #     # password: "please use keys"
 #   }
 
-server "52.196.148.45", user: "ec2-user", roles: %w{app db web}
+server "52.197.142.85", user: "ec2-user", roles: %w{app db web}

c11c648117ec4c5e5511139d90dbb9c0fdfb3fad
コミットしました <yumayo14@gmail.com>
2018-03-08 19:58:30 +0900

    キャリアモデル、カンパニーモデルへの処理の書き出し

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,16 @@
 class Company < ApplicationRecord
   has_many :careers
   has_many :users, through: :careers
+
+  #Usersコントローラにおける処理の切り出し
+  # 以下の条件分岐は、名刺情報編集というformで情報を保存する際に、companiesテーブルとcareersテーブルの情報を保存した上で、他のユーザー情報更新の際に、正しくformを動作させるために必要
+    def company_update(company_params)
+      if company_params
+        if self.present?
+          self.update(company_params)
+        end
+      end
+    end
+
+
 end
